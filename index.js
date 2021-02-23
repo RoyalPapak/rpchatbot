1e1787b3f77a015f51e1e27b78125da78f65a9fa
RoyalPapak <54402795+RoyalPapak@users.noreply.github.com>
2021-02-23 19:28:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+// first of all u must install these 1)https://code.visualstudio.com/ 2)https://nodejs.org/en/
+// make a folder for your bot example fivem bot
+// after installing these apps open command prompt(cmd) and go to your folder for example cd Desktop/fivem bot
+// write npm init and then press enter to all(including the yes in the end)
+// then you have to install the packages that we need to use 
+// run npm i discord.js
+// after installing the package go to visual studio code open the fivem bot folder create a file named index.js and paste the code from here
+// open this site (https://discord.com/developers/applications) and make a new application and make it as a bot
+// if you want to add the bot in your server just go here (https://discordapi.com/permissions.html) select the administrator button and put in the client id your bots id
+// then copy the link and select your server 
+// Video: https://www.youtube.com/watch?v=j_sD9udZnCk
+// Join here for help: https://discord.gg/fSRtfz7Rn3 || https://discord.gg/7seSqGm6mC
+// Install npm's : npm i npmnamehere
 const Discord = require("discord.js") // npm i discord.js
 const bot = new Discord.Client()
-const config = require("./config.json")
+const config = require("./config.json") // You must have the config.json file(Just check the folder)
 
 bot.on("ready", () =>{
     console.log("RP Chat Bot is ON | Made by Papak†#9999")
@@ -147,5 +160,5 @@ bot.on("message", message => {
         }
       } 
 })
-bot.login("NzYwOTE2MDg5NTcxNTA4Mjk0.X3TAVA.z4We6LpzfNV4K89CmEbA2Mk2FUQ")
+bot.login("token here") // you will find it at (https://discord.com/developers/applications)
 // RP Chat Bot Made by Papak†#9999
